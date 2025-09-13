dc813e0baea995ff205fc9f130d5bdf59bc40f82
Km103 <mittalkanishak9@gmail.com>
2025-09-13 15:23:30 +0530

    implementing cursor

--- src/include/table.h
+++ src/include/table.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 #include <stdlib.h>
-
+#include<stdbool.h>
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
 #define TABLE_MAX_PAGES 100
@@ -25,12 +25,22 @@ typedef struct {
     Pager*   pager;
 } Table;
 
+typedef struct{
+    Table *table;
+    uint32_t row_num;
+    bool end_of_table;
+}Cursor;
+
+Cursor* table_start(Table* table);
+Cursor* table_end(Table* table);
+
 Table* db_open(const char* filename);
 Pager* pager_open(const char* filename);
 void   db_close(Table* table);
 void   serialize_row(Row* source, void* destination);
 void   deserialize_row(void* source, Row* destination);
-void*  row_slot(Table* table, uint32_t row_num);
+void* cursor_value(Cursor * cursor);
+void cursor_advance(Cursor *cursor);
 void   print_row(Row* row);
 
 extern const uint32_t TABLE_MAX_ROWS;
